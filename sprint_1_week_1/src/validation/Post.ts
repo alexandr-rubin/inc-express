aef90f5defdcac509eb4833ea28df131b582c328
Alexander Rubin <alexandrrubin0715@gmail.com>
2023-05-04 17:12:15 +0300

    fix validation

--- sprint_1_week_1/src/validation/Post.ts
+++ sprint_1_week_1/src/validation/Post.ts
@@ -2,9 +2,9 @@ import { body } from "express-validator"
 import { blogRepository } from "../repositories/blog-respository"
 
 export const validatePost = [
-    body('title').isLength({min: 1, max: 30}),
-    body('shortDescription').isLength({min: 1, max: 100}),
-    body('content').isLength({min: 1, max: 1000}),
+    body('title').trim().isLength({min: 1, max: 30}),
+    body('shortDescription').trim().isLength({min: 1, max: 100}),
+    body('content').trim().isLength({min: 1, max: 1000}),
     body('blogId').custom(async id => {
         const blog = await blogRepository.getBlogById(id)
         if (!blog) {
