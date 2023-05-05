ada5cfdba444849d4a301c190356a00671ac2dee
Alexander Rubin <alexandrrubin0715@gmail.com>
2023-05-05 13:36:15 +0300

    fix validation

--- sprint_1_week_1/dist/middlewares/validation-errors-handler.js
+++ sprint_1_week_1/dist/middlewares/validation-errors-handler.js
@@ -3,7 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.validationErrorsHandler = void 0;
 const express_validator_1 = require("express-validator");
 const validationErrorsHandler = (req, res, next) => {
-    const errorsMessages = (0, express_validator_1.validationResult)(req).array({ onlyFirstError: true }).map(error => ({ message: error.msg, field: error.path }));
+    const errorsMessages = (0, express_validator_1.validationResult)(req).array({ onlyFirstError: true }).map(error => ({ message: error.msg, field: error.type === 'field' ? error.path : error.type }));
+    const error = (0, express_validator_1.validationResult)(req).array();
     if (errorsMessages.length) {
         res.status(400).json({ errorsMessages });
     }
--- sprint_1_week_1/src/middlewares/validation-errors-handler.ts
+++ sprint_1_week_1/src/middlewares/validation-errors-handler.ts
@@ -2,7 +2,8 @@ import { NextFunction, Request, Response } from "express"
 import { validationResult } from "express-validator"
 
 export const validationErrorsHandler = (req: Request, res: Response, next: NextFunction) => {
-    const errorsMessages = validationResult(req).array({ onlyFirstError: true }).map(error => ({ message: error.msg, field: error.path }))
+    const errorsMessages = validationResult(req).array({ onlyFirstError: true }).map(error => ({ message: error.msg, field: error.type === 'field' ? error.path : error.type }))
+    const error = validationResult(req).array()
     if (errorsMessages.length) {
         res.status(400).json({ errorsMessages });
     }
