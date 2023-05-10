4900f505f56405b4aa7d49d013cf6e20d0606c53
Alexander Rubin <alexandrrubin0715@gmail.com>
2023-05-10 17:22:27 +0300

    fix index.ts

--- sprint_1_week_1/src/index.ts
+++ sprint_1_week_1/src/index.ts
@@ -2,12 +2,8 @@ import { app } from './setting'
 import { runDb } from './repositories/db'
 
 const port = process.env.PORT || 3000
+runDb()
 
-const startApp = async () => {
-  await runDb()
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
-  })
-}
-
-startApp()
+})
