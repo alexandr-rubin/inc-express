ca9254291738c18099812d7f3120999dd242300f
Alexander Rubin <alexandrrubin0715@gmail.com>
2023-05-10 17:09:25 +0300

    fix run db

--- sprint_1_week_1/src/index.ts
+++ sprint_1_week_1/src/index.ts
@@ -3,11 +3,8 @@ import { runDb } from './repositories/db'
 
 const port = process.env.PORT || 3000
 
-export const server = async () => {
-  await runDb()
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-  })
-}
+runDb()
 
-server()
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
