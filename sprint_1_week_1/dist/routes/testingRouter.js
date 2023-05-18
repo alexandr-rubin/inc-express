b03a7c3a4fe845a3cdff4167e6419f80274f0ad5
Alexander Rubin <alexandrrubin0715@gmail.com>
2023-05-18 18:42:56 +0300

    fix

--- sprint_1_week_1/dist/routes/testingRouter.js
+++ sprint_1_week_1/dist/routes/testingRouter.js
@@ -5,10 +5,12 @@ const express_1 = require("express");
 const video_router_1 = require("./video-router");
 const blogRespository_1 = require("../repositories/blogRespository");
 const postRespository_1 = require("../repositories/postRespository");
+const userRepository_1 = require("../repositories/userRepository");
 exports.testingRouter = (0, express_1.Router)({});
 exports.testingRouter.delete('/', (req, res) => {
     (0, video_router_1.testingDeleteAllVideos)();
     blogRespository_1.blogRepository.testingDeleteAllBlogs();
     postRespository_1.postRepository.testingDeleteAllPosts();
+    userRepository_1.userRepository.testingDeleteAllUsers();
     res.status(204).send('All data is deleted');
 });
--- sprint_1_week_1/src/routes/testingRouter.ts
+++ sprint_1_week_1/src/routes/testingRouter.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from "express"
 import { testingDeleteAllVideos } from "./video-router"
 import { blogRepository } from "../repositories/blogRespository"
 import { postRepository } from "../repositories/postRespository"
+import { userRepository } from "../repositories/userRepository"
 
 export const testingRouter = Router({})
 
@@ -9,5 +10,6 @@ testingRouter.delete('/', (req: Request, res: Response) => {
     testingDeleteAllVideos()
     blogRepository.testingDeleteAllBlogs()
     postRepository.testingDeleteAllPosts()
+    userRepository.testingDeleteAllUsers()
     res.status(204).send('All data is deleted') 
 })
